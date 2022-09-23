16eb778afad75923ff2f5aa41575118975463f18
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2022-09-23 15:16:21 -0700

    Fix weight row case sensitivity.

--- asl-project/api/src/seeders/20220619034108-add-quizzes.js
+++ asl-project/api/src/seeders/20220619034108-add-quizzes.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict';
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     queryInterface.bulkInsert(
-      "Quizzes",
+      'Quizzes',
       [
         {
-          name: "Database Abstraction Layers",
-          weight: 25,
+          name: 'Database Abstraction Layers',
+          Weight: 25,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "Object Relational Mappers",
-          weight: 25,
+          name: 'Object Relational Mappers',
+          Weight: 25,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "Using Migrations",
-          weight: 25,
+          name: 'Using Migrations',
+          Weight: 25,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "RESTful Design Patters",
-          weight: 25,
+          name: 'RESTful Design Patters',
+          Weight: 25,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -35,12 +35,12 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Quizzes", {
+    await queryInterface.bulkDelete('Quizzes', {
       [Sequelize.Op.or]: [
-        { name: "Database Abstraction Layers" },
-        { name: "Object Relational Mappers" },
-        { name: "Using Migrations" },
-        { name: "RESTful Design Patters" },
+        { name: 'Database Abstraction Layers' },
+        { name: 'Object Relational Mappers' },
+        { name: 'Using Migrations' },
+        { name: 'RESTful Design Patters' },
       ],
     });
   },
